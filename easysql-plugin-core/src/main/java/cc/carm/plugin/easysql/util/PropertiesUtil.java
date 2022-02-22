2345f3567f4faf368497701425c4313722a5a8a6
CarmJos <KarmunJ@outlook.com>
2022-02-23 03:14:33 +0800

    完成 properties 读取工具

--- easysql-plugin-core/src/main/java/cc/carm/plugin/easysql/util/PropertiesUtil.java
+++ easysql-plugin-core/src/main/java/cc/carm/plugin/easysql/util/PropertiesUtil.java
@@ -1,6 +1,9 @@
 package cc.carm.plugin.easysql.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -9,7 +12,21 @@ public class PropertiesUtil {
 
     public static Map<String, Properties> readDBProperties(File propertiesFolder) {
         Map<String, Properties> propertiesMap = new HashMap<>();
+        if (!propertiesFolder.isDirectory()) return propertiesMap;
 
+        File[] files = propertiesFolder.listFiles();
+        if (files == null || files.length == 0) return propertiesMap;
+        for (File file : files) {
+            if (file.getName().startsWith(".") || !file.getName().endsWith(".properties")) continue;
+            String name = file.getName().substring(0, file.getName().lastIndexOf("."));
+            try (InputStream stream = new FileInputStream(file)) {
+                Properties properties = new Properties();
+                properties.load(stream);
+                propertiesMap.put(name, properties);
+            } catch (IOException ignored) {
+            }
+        }
+        
         return propertiesMap;
     }
 
