668df1d5179b5f02f04dfda5baebff54f1b830f0
carm <carm@carm.cc>
2022-12-20 09:45:42 +0800

    chore(conf): 修改配置文件注释

--- core/src/main/java/cc/carm/plugin/minesql/conf/PluginConfiguration.java
+++ core/src/main/java/cc/carm/plugin/minesql/conf/PluginConfiguration.java
@@ -8,16 +8,16 @@ import cc.carm.lib.configuration.core.value.type.ConfiguredValue;
 public class PluginConfiguration extends ConfigurationRoot {
 
     @HeaderComment("排错模式，一般留给开发者检查问题，平常使用无需开启。")
-    public ConfigValue<Boolean> DEBUG = ConfiguredValue.of(Boolean.class, false);
+    public final ConfigValue<Boolean> DEBUG = ConfiguredValue.of(Boolean.class, false);
 
-    @HeaderComment({
+    @HeaderComment({"",
             "统计数据设定",
             "该选项用于帮助开发者统计插件版本与使用情况，且绝不会影响性能与使用体验。",
             "当然，您也可以选择在这里关闭，或在plugins/bStats下的配置文件中关闭所有插件的统计信息。"
     })
-    public ConfigValue<Boolean> METRICS = ConfiguredValue.of(Boolean.class, true);
+    public final ConfigValue<Boolean> METRICS = ConfiguredValue.of(Boolean.class, true);
 
-    @HeaderComment({
+    @HeaderComment({"",
             "检查更新设定",
             "该选项用于插件判断是否要检查更新，若您不希望插件检查更新并提示您，可以选择关闭。",
             "检查更新为异步操作，绝不会影响性能与使用体验。"
@@ -25,15 +25,15 @@ public class PluginConfiguration extends ConfigurationRoot {
     public ConfigValue<Boolean> UPDATE_CHECKER = ConfiguredValue.of(Boolean.class, true);
 
     @HeaderComment({"插件注册池配置"})
-    public SettingsConfig SETTINGS = new SettingsConfig();
+    public final SettingsConfig SETTINGS = new SettingsConfig();
 
-    @HeaderComment({
+    @HeaderComment({"",
             "Properties 数据库配置文件配置",
             "相关配置介绍(BeeCP) https://github.com/Chris2018998/BeeCP/wiki/Configuration--List#配置列表"
     })
-    public PropertiesConfig PROPERTIES = new PropertiesConfig();
+    public final PropertiesConfig PROPERTIES = new PropertiesConfig();
 
-    @HeaderComment({
+    @HeaderComment({"",
             "数据库源配置",
             "目前支持的驱动类型(type)有 mariadb、mysql、h2-file(文件数据库) 与 h2-mem(内存临时数据库)。",
             "详细配置介绍请查看 https://github.com/CarmJos/MineSQL/.doc/README.md"
@@ -47,7 +47,7 @@ public class PluginConfiguration extends ConfigurationRoot {
 
     public static class PropertiesConfig extends ConfigurationRoot {
 
-        @HeaderComment("该选项用于启用 Properties 配置读取，若您不希望插件启用 Properties 文件配置，可以选择关闭。")
+        @HeaderComment({"该选项用于启用 Properties 配置读取。", "若您不希望插件启用 Properties 文件配置功能，可以选择关闭。"})
         public ConfigValue<Boolean> ENABLE = ConfiguredValue.of(Boolean.class, true);
 
         @HeaderComment({
